ebfc607f7ff9da002ef4161542e60e9d00a71b8a
singhankur7 <46572222+singhankur7@users.noreply.github.com>
2019-02-01 11:48:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Music gives a soul to universe
+
+"""
+
+
+#Martin garrix
+SongName = "There For You"         #declare the name of the song in variable name SongName
+Artist = "Martin Garrix, Troye Sivan"    #declare the artist
+Genre = "EDM"                            #declare the genre of the song
+Duration = 3.41                          #the length of the song
+Lyricist = "Troye Sivan, Jessie Thomas"    #song is written by
+Released = 2017                            #song relesed in which year
+
+print("Song name is: " +SongName)
+print("Artist name is: " +Artist)
+print("Genre of song is: " +Genre)
+print(str(Duration) + " is the Duration of the song")
+print("Song is written by: " +Lyricist)
+print(str(Released) + " is the year in which this song is released")
+
+print("")
+
+#vilen
+SongName = "Ek raat"
+Artist = "Vilen"
+Genre = "Vocal"
+Duration = 3.45
+Lyricist = "Vilen"
+Released = 2018
+
+print("Song name is: " +SongName)
+print("Artist name is: " +Artist)
+print("Genre of the song is: " +Genre)
+print(str(Duration) + " is the Duration of the song")
+print("Song is written by: " +Lyricist)
+print(str(Released) + " is the year in which this song is released")
