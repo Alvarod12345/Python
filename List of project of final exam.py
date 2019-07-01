5bc265f6ab733a951c7847b969fdc6dcf1fa13a1
Ankur Singh <singh.ankur113@gmail.com>
2019-07-01 22:59:00 +0530

    Add files via upload

---
+++ List of project of final exam.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 30 21:22:09 2019
+
+@author: Ankur Singh
+"""
+
+
+***(This is my favourite one and i would like to add some more features to this)
+***1.https://github.com/PatrycjaOwczarek/PythonBasic/blob/master/Calculator/calculator.py
+
+
+(Its a game that i used to play so it would we good to contribute to it)
+2.https://github.com/clairebroderick/BasicPythonExamples/blob/master/Games/Rock%20Paper%20Scisors.py
+
+(This wifi password project looks interesting and I would like to contribute to it)
+3. https://pypi.org/project/wifiPassword
+
+(Can add one more condition to check leap year)
+4.https://github.com/RahulDhiman93/pythonProjects/blob/master/leap_year.py
+
+(To add more details to it)
+5.https://github.com/mvskiren/PythonProjects/blob/master/ticketapp.py
